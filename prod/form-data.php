a0addf3ac57044001b9d2d7e9f639b6fa4ff8a40
lex0819 <lexmolnar@gmail.com>
2024-01-08 21:32:47 +0300

    js form send async await

--- prod/form-data.php
+++ prod/form-data.php
@@ -2,10 +2,15 @@
 // Если форма отправлена, обрабатываем данные
 if (isset($_POST['email'])) {
   // Проверяем, являются ли поля валидными
-  if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+  $email = trim($_POST['email']);
+
+  if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
     echo 'Please enter right email !';
     die;
   } else {
+    /**
+     * text file
+     *
     // Открываем файл для записи
     $file = fopen('data-email.txt', 'a');
 
@@ -14,6 +19,21 @@ if (isset($_POST['email'])) {
 
     // Закрываем файл
     fclose($file);
+    */
+    
+    /**
+     * excel fale
+     */
+    // формируем строку с данными
+    $csv_data = array('date' => date('Y-m-d H:i:s'), 'email' => $email);
+    // Открываем файл для записи
+    $file_handle = fopen('email.csv', 'a');
+    // Записываем данные в файл
+    foreach ($csv_data as $key => $value) {
+        fputcsv($file_handle, array($value));
+    }
+    // Закрываем файл
+    fclose($file_handle);
 
     echo 'Sucsess!';
   }
